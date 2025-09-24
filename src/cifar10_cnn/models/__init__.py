7895f5bfe02bd65e9d1293f92984ac403b811f9f
MayurBhama <mayurbhama9@gmail.com>
2025-09-24 10:31:12 +0000

    chore: initial scaffold with src layout

--- src/cifar10_cnn/models/__init__.py
+++ src/cifar10_cnn/models/__init__.py
@@ -2,3 +2,4 @@ from .cnn import create_cifar10_cnn
 from .cnn import create_cifar10_cnn
 from .cnn import create_cifar10_cnn
 from .cnn import create_cifar10_cnn
+from .cnn import create_cifar10_cnn
