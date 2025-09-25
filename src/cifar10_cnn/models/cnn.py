314fc4ea847057387f86d41157246d2605497066
MayurBhama <mayurbhama9@gmail.com>
2025-09-25 08:15:27 +0000

    feat: add CNN basic model architecture module

--- src/cifar10_cnn/models/cnn.py
+++ src/cifar10_cnn/models/cnn.py
@@ -1,5 +1,5 @@
-import tensorflow as tf 
-from tensorflow.keras import layers, models 
+import tensorflow as tf
+from tensorflow.keras import layers, models
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D
 from keras.layers import Dropout, Flatten, BatchNormalization
@@ -9,13 +9,13 @@ from keras.callbacks import EarlyStopping
 
 def create_cifar10_cnn(input_shape = (32, 32, 3), num_classes = 10):
   """
-  Creating a simple CNN model 
+  Creating a simple CNN model
   """
   model = Sequential()
 
   #Convolution block 1
-  model.add(Conv2D(32, (3, 3), activation = 'relu', padding = 'same', input_shape = input_shape)) 
-  #Adding maxpooling layer 
+  model.add(Conv2D(32, (3, 3), activation = 'relu', padding = 'same', input_shape = input_shape))
+  #Adding maxpooling layer
   model.add(MaxPooling2D((2,2)))
 
   #Convolution block 2
@@ -25,13 +25,13 @@ def create_cifar10_cnn(input_shape = (32, 32, 3), num_classes = 10):
   #Convolution block 3
   model.add(Conv2D(64, (3,3), activation = 'relu', padding = 'same'))
 
-  #Flatten the layer 
+  #Flatten the layer
   model.add(Flatten())
-  
-  #Adding hidden or dense layer 
+
+  #Adding hidden or dense layer
   model.add(Dense(64, activation = 'relu'))
 
-  #Final output layer 
+  #Final output layer
   model.add(Dense(num_classes, activation = 'softmax'))
 
-  return model 
+  return model
